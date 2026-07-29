b356a5a2264345dbf24e9dd02df9cd941b567444
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-07-29 08:12:11 -0300

    Ativa acabamento visual da área Git

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -9,6 +9,7 @@ import './project-details-redesign.css';
 import './log-visual-enhancer.css';
 import './sql-explanation-enhancer.css';
 import './log-detail-enhancer.css';
+import './git-modern-polish.css';
 import { installLogDetailEnhancer } from './log-detail-enhancer';
 import { installLogVisualEnhancer } from './log-visual-enhancer';
 import { installSqlExplanationEnhancer } from './sql-explanation-enhancer';
