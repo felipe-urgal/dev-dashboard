d3e10bc9418d86db4799643c558f20a52867acba
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-08-03 14:07:52 -0300

    Alinha mocks aos workers explícitos do Monaco

--- apps/web/test/project-embedded-editor.test.ts
+++ apps/web/test/project-embedded-editor.test.ts
@@ -38,11 +38,11 @@ vi.mock('monaco-editor', () => ({
 }));
 
 for (const worker of [
-  'monaco-editor/esm/vs/editor/editor.worker?worker',
-  'monaco-editor/esm/vs/language/css/css.worker?worker',
-  'monaco-editor/esm/vs/language/html/html.worker?worker',
-  'monaco-editor/esm/vs/language/json/json.worker?worker',
-  'monaco-editor/esm/vs/language/typescript/ts.worker?worker',
+  'monaco-editor/esm/vs/editor/editor.worker.js?worker',
+  'monaco-editor/esm/vs/language/css/css.worker.js?worker',
+  'monaco-editor/esm/vs/language/html/html.worker.js?worker',
+  'monaco-editor/esm/vs/language/json/json.worker.js?worker',
+  'monaco-editor/esm/vs/language/typescript/ts.worker.js?worker',
 ]) {
   vi.mock(worker, () => ({ default: class MockWorker {} }));
 }
