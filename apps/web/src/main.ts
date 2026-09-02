76be078270a9f6abd1461d32e8117f98ae7b6c97
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-09-02 17:09:59 -0300

    refactor(tests): remove Test Inspector imperativo (#532)

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -9,7 +9,6 @@ import './project-dependencies-typography.css';
 import './database-layout-polish.css';
 import './scripts-explorer-redesign.css';
 import './test-log-theme-fix.css';
-import './test-log-inspector.css';
 import './git-modern-polish.css';
 import './git-inline-diff-theme.css';
 import './git-diff-compact-enhancer.css';
@@ -19,18 +18,9 @@ import './git-syntax-highlight.css';
 import './sidebar-collapse.css';
 import './shell-option3.css';
 import { installTestLogAutoFollow } from './test-log-auto-follow';
-import { installTestLogInspector } from './test-log-inspector';
-import { installTestLogInspectorMutationGuard } from './test-log-inspector-mutation-guard';
 import { loadVisualPreferences } from './utils/visual-preferences';
 
 loadVisualPreferences();
 installTestLogAutoFollow();
-const restoreTestLogInspectorMutationObserver =
-  installTestLogInspectorMutationGuard();
-try {
-  installTestLogInspector();
-} finally {
-  restoreTestLogInspectorMutationObserver();
-}
 
 createApp(App).use(router).mount('#app');
--- apps/web/src/test-log-inspector/constants.ts
+++
@@ -1,3 +0,0 @@
-export const ANSI_PATTERN = /\u001B\[[0-?]*[ -/]*[@-~]/g;
-export const STACK_PATH_PATTERN =
-  /((?:\.{0,2}\/|[A-Za-z]:\\)[^\s:]+):(\d+)(?::\d+)?/;
