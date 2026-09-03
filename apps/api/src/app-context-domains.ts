598f862f8304b7b9bc378e2ba32f5c88d1e2484a
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-09-03 09:15:26 -0300

    style: formata domínios do AppContext

--- apps/api/src/app-context-domains.ts
+++ apps/api/src/app-context-domains.ts
@@ -97,9 +97,13 @@ export function createExecutionContextDomain(processManager: ProcessManager) {
 
   return {
     scriptDetectionService,
-    scriptExecutionService: new ScriptExecutionService(scriptDetectionService),
+    scriptExecutionService: new ScriptExecutionService(
+      scriptDetectionService,
+    ),
     testDetectionService,
-    testExecutionHistoryService: new TestExecutionHistoryService(processManager),
+    testExecutionHistoryService: new TestExecutionHistoryService(
+      processManager,
+    ),
     detachableExecutionService,
     projectTestPtyService: new ProjectTestPtyService(
       detachableExecutionService,
